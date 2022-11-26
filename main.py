b9c28f8574923dca5d3074f04e328153795f668f
aflynn0309 <107883034+aflynn0309@users.noreply.github.com>
2022-11-26 14:36:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import random
+import time
+import os
 
-num = random.randint(0, 1000)
-
-guess = int(input("Enter a number from 0 - 1000. Press enter to see if it was correct"))
-if guess == num:
-    print("Correct! You Guessed the Right Number!")
-    input('press enter to exit')
-else:
-    print("Incorrect! You Guessed Wrong! :(")
-    print("The Correct Number Is: ")
-    print(num)
-    input('press enter to exit')
+while True:
+    num = random.randint(0, 1000)
+    os.system('cls')
+    guess = int(input("Enter a number from 0 - 1000. Press enter to see if it was correct"))
+    if guess == num:
+        print("Correct! You Guessed the Right Number!")
+        input('press enter to exit')
+        time.sleep(1.5)
+    else:
+        print("Incorrect! You Guessed Wrong! :(")
+        print("The Correct Number Is: ")
+        print(num)
+        time.sleep(1.5)
